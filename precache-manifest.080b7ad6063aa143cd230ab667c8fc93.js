2739ea809ff959ac6967b5e598c5c019cac09ae4
Trinad <trinad536@yahoo.com>
2019-10-06 20:38:04 +0530

    Updates

---
+++ precache-manifest.080b7ad6063aa143cd230ab667c8fc93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bd0940f29d751b81035964b9a41b36",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "645ab282bdc89879f5f3",
+    "url": "/monsters-rolodex/static/css/main.b2826484.chunk.css"
+  },
+  {
+    "revision": "56022eb4a5dc281792e1",
+    "url": "/monsters-rolodex/static/js/2.66e5ab65.chunk.js"
+  },
+  {
+    "revision": "645ab282bdc89879f5f3",
+    "url": "/monsters-rolodex/static/js/main.9b8c1cc1.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
